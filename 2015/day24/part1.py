7a90a08b13bb6808dd4782c58e74bafa3aeccaff
Alejandro <acarrasco@localhost.localdomain>
2023-11-06 16:09:05 +0100

    Year 2015 - Day 24
    Another easy one.
    
    I was concerned this would require to be solved with dynamic
    programming like a knapsack problem (which I always struggle with),
    but it was solvable with backtracking and a bit of prunning.

---
+++ 2015/day24/part1.py
@@ -0,0 +1,41 @@
+import sys
+
+from pprint import pprint
+
+weights = sorted(map(int, sys.stdin), reverse=True)
+total = sum(weights)
+
+target = total // 3
+
+def prod(seq):
+    p = 1
+    for n in seq:
+        p *= n
+    return p
+
+def find_groups_of_weight(candidates, remaining_weights, remaining_sum, target):    
+    if target == 0:
+        yield candidates
+    elif remaining_sum < target:
+        return
+    else:
+        for i, w in enumerate(remaining_weights):
+            remaining_sum -= w
+            if w <= target:
+                next_candidates = candidates + (w,)
+                next_remaining_weights = remaining_weights[i+1:]
+                next_target = target - w
+                yield from find_groups_of_weight(next_candidates,
+                                                 next_remaining_weights,
+                                                 remaining_sum,
+                                                 next_target)
+
+group_candidates = find_groups_of_weight((), weights, total, target)
+ranked_candidates = sorted(group_candidates, key=lambda g: (len(g), prod(g)))
+
+for g in ranked_candidates:
+    remaining_weights = sorted(set(weights) - set(g), reverse=True)
+    side_groups = find_groups_of_weight((), remaining_weights, target*2, target)
+    if any(side_groups):
+        print(prod(g))
+        sys.exit()
