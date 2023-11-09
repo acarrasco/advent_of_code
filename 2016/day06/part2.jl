dc86fbde9a5ac67de910aa25b43d2188e7438dcc
Alejandro <acarrasco@localhost.localdomain>
2023-11-09 22:31:28 +0100

    Year 2016 - Day 6
    Boring problem, keep moving.

---
+++ 2016/day06/part2.jl
@@ -0,0 +1,18 @@
+function leastRepeated(d::Dict{Char, Int})::Char
+    return sort(collect(d), by=x->x.second)[1].first
+end
+
+function solve(lines)::String
+    n = length(lines[1])
+    counts = [Dict{Char, Int}() for _ = 1:n]
+
+    for line in lines
+        for (d, c) in zip(counts, line)
+            d[c] = get(d, c, 0) + 1
+        end
+    end
+
+    return join(leastRepeated(d) for d in counts)
+end
+
+println(solve(readlines()))
