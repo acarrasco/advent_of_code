8a6abca1423d0e9787a0cbda9772bd95ec6bd037
Alejandro Carrasco <alejandro.carrasco@gmail.com>
2023-12-11 08:41:48 +0100

    Year 2023 - day 11
    It was taking a bit too long because all the unnecessary recalculations,
    so I made a optimization precalculating the offsets for each row and
    column.
    
    Execution time went from 0.4s to 0.05s.

--- 2023/day11/part2.py
+++ 2023/day11/part2.py
@@ -4,7 +4,23 @@ import sys
 def parse_input(lines):
     return [list(line.strip()) for line in lines]
 
-def find_galaxies(lines):
+def calculate_offsets(size, expansion_factor, occupied):
+    offsets = []
+    expanded = 0
+    for i in range(size):
+        offsets.append(expanded)
+        if i in occupied:
+            expanded += 1
+        else:
+            expanded += expansion_factor
+    return offsets
+
+def calculate_row_and_column_offsets(rows, columns, rows_with_galaxies, columns_with_galaxies, expansion_factor):
+    row_offsets = calculate_offsets(rows, expansion_factor, rows_with_galaxies)
+    column_offsets = calculate_offsets(columns, expansion_factor, columns_with_galaxies)
+    return row_offsets, column_offsets
+
+def find_galaxies_and_offsets(lines, expansion_factor):
     galaxies = []
     rows_with_galaxies = set()
     columns_with_galaxies = set()
@@ -16,37 +32,21 @@ def find_galaxies(lines):
                 rows_with_galaxies.add(i)
                 columns_with_galaxies.add(j)
 
-    return galaxies, rows_with_galaxies, columns_with_galaxies
+    rows = len(lines)
+    cols = len(lines[0])
+    return galaxies, calculate_row_and_column_offsets(rows, cols, rows_with_galaxies, columns_with_galaxies, expansion_factor)
 
-
-def calculate_distance(a, b, rows_with_galaxies, columns_with_galaxies, expansion_factor):
-    total = 0
-    min_row = min(a[0], b[0])
-    max_row = max(a[0], b[0])
-    for row in range(min_row, max_row):
-        if row in rows_with_galaxies:
-            total += 1
-        else:
-            total += expansion_factor
-
-    min_col = min(a[1], b[1])
-    max_col = max(a[1], b[1])
-    for col in range(min_col, max_col):
-        if col in columns_with_galaxies:
-            total += 1
-        else:
-            total += expansion_factor
-    return total
 
 def calculate_distances(universe, expansion_factor=1000000):
-
-    galaxies, rows_with_galaxies, columns_with_galaxies = find_galaxies(universe)
+    galaxies, (row_offsets, column_offsets) = find_galaxies_and_offsets(universe, expansion_factor)
     
     total = 0
-    for i, a in enumerate(galaxies):
-        for j, b in enumerate(galaxies):
-            if j > i:
-                d = calculate_distance(a, b, rows_with_galaxies, columns_with_galaxies, expansion_factor)
+    for ga, (gai, gaj) in enumerate(galaxies):
+        for gb, (gbi, gbj) in enumerate(galaxies):
+            if gb > ga:
+                real_ai, real_aj = row_offsets[gai], column_offsets[gaj]
+                real_bi, real_bj = row_offsets[gbi], column_offsets[gbj]
+                d = abs(real_ai - real_bi) + abs(real_aj - real_bj)
                 total += d
     return total
 
