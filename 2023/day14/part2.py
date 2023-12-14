7b91e5e15cfabd904ff3fcf4156651917d28d8e5
Alejandro Carrasco <alejandro.carrasco@gmail.com>
2023-12-14 11:16:51 +0100

    Year 2023 - day 14
    I was unhappy with the performance of part 2, so I rewrote it to use
    a sparse representation and do rotations instead of coding each tilt
    as a separate function.
    
    Doing the rotations also simplifies the code a bit because the sparse
    representation is row-major; so iterating over columns would had been
    a bit more cumbersome.
    
    Execution time went from 10s to 0.4s

--- 2023/day14/part2.py
+++ 2023/day14/part2.py
@@ -1,90 +1,112 @@
 import sys
+from collections import defaultdict
 
 EMPTY = "."
 FIXED_ROCK = "#"
 MOVING_ROCK = "O"
 
 def parse_input(lines):
-    return list(list(line.strip()) for line in lines)
+    lines = [line.strip() for line in lines if line.strip()]
 
-def tilt_north(problem):
-    moved = True
-    while moved:
-        moved = False
-        for up, bottom in zip(problem, problem[1:]):
-            for j, (u, b) in enumerate(zip(up, bottom)):
-                if u is EMPTY and b is MOVING_ROCK:
-                    up[j] = MOVING_ROCK
-                    bottom[j] = EMPTY
-                    moved = True
+    fixed_rocks = defaultdict(list)
+    moving_rocks = defaultdict(list)
+    for i, line in enumerate(lines):
+        for j, x in enumerate(line):
+            if x is FIXED_ROCK:
+                fixed_rocks[i].append(j)
+            elif x is MOVING_ROCK:
+                moving_rocks[i].append(j)
+    return fixed_rocks, moving_rocks, len(lines), len(lines[0])
 
-def tilt_south(problem):
-    moved = True
-    while moved:
-        moved = False
-        for up, bottom in zip(problem, problem[1:]):
-            for j, (u, b) in enumerate(zip(up, bottom)):
-                if u is MOVING_ROCK and b is EMPTY:
-                    up[j] = EMPTY
-                    bottom[j] = MOVING_ROCK
-                    moved = True
+def tilt_north(fixed_rocks, moving_rocks):
+    result = defaultdict(list)
+    max_north_clearance = defaultdict(lambda: -1)
+    upper_row_with_fixed_rocks = min(fixed_rocks, default=0)
+    for col in fixed_rocks[upper_row_with_fixed_rocks]:
+        max_north_clearance[col] = upper_row_with_fixed_rocks
 
-def tilt_west(problem):
-    moved = True
-    while moved:
-        moved = False
-        for row in problem:
-            for j, x in enumerate(row[1:], 1):
-                if x is MOVING_ROCK and row[j-1] is EMPTY:
-                    row[j-1] = MOVING_ROCK
-                    row[j] = EMPTY
-                    moved = True
+    all_rows = set(fixed_rocks.keys()) | set(moving_rocks.keys())
 
-def tilt_east(problem):
-    moved = True
-    while moved:
-        moved = False
-        for row in problem:
-            for j, x in enumerate(row[1:], 1):
-                if x is EMPTY and row[j-1] is MOVING_ROCK:
-                    row[j-1] = EMPTY
-                    row[j] = MOVING_ROCK
-                    moved = True
+    for row in sorted(all_rows):
+        for col in moving_rocks.get(row, ()):
+            destination_row = max_north_clearance[col] + 1
+            result[destination_row].append(col)
+            max_north_clearance[col] = destination_row
+        for col in fixed_rocks.get(row, ()):
+            max_north_clearance[col] = row
+    return result
 
-def tilt(problem):
-    tilt_north(problem)
-    tilt_west(problem)
-    tilt_south(problem)
-    tilt_east(problem)
+def rotate_90_clockwise(sparse_matrix, rows, cols):
+    result = defaultdict(list)
+    for i in sparse_matrix:
+        for j in sparse_matrix[i]:
+            result[j].append(rows - i - 1)
+    return result, cols, rows
 
-def count_load(problem):
-    n = len(problem)
+def tilt_and_rotate_360(fixed_rocks_by_orientation, moving_rocks, rows, cols):
+    for fixed_rocks in fixed_rocks_by_orientation:
+        moving_rocks = tilt_north(fixed_rocks, moving_rocks)
+        moving_rocks, rows, cols = rotate_90_clockwise(moving_rocks, rows, cols)
+
+    return moving_rocks
+
+def calculate_load(moving_rocks, rows):
     load = 0
-    for i, row in enumerate(problem):
-        south_rows = n - i
-        load += sum(south_rows for x in row if x is MOVING_ROCK)
+    for row in moving_rocks:
+        south_rows = rows - row
+        load += south_rows * len(moving_rocks[row])
     return load
 
-def problem_to_hashable(problem):
-    return tuple("".join(row) for row in problem)
+def sparse_to_hashable(sparse_matrix):
+    def row_to_hashable(row):
+        return f'{row}:{",".join(map(str, sparse_matrix[row]))}'
+    return ';'.join(row_to_hashable(row) for row in sorted(sparse_matrix))
 
-TARGET_CYCLES = 1000000000
-def solve(problem):
+def hashable_to_sparse(hashable):
+    sparse_matrix = defaultdict(list)
+    for row in hashable.split(';'):
+        row, cols = row.split(':')
+        sparse_matrix[int(row)] = list(map(int, cols.split(',')))
+    return sparse_matrix
+
+def problem_to_input(fixed_rocks, moving_rocks, rows, cols):
+    out = []
+    def get_char(i, j):
+        if i in moving_rocks and j in moving_rocks[i]:
+            return MOVING_ROCK
+        if i in fixed_rocks and j in fixed_rocks[i]:
+            return FIXED_ROCK
+        return EMPTY
+    for i in range(rows):
+        out.append(''.join(get_char(i,j) for j in range(cols)))
+    return '\n'.join(out)
+
+def calculate_fixed_rocks_rotations(fixed_rocks, rows, cols):
+    fixed_rocks_by_orientation = [fixed_rocks]
+    for _ in range(3):
+        fixed_rocks, rows, cols = rotate_90_clockwise(fixed_rocks, rows, cols)
+        fixed_rocks_by_orientation.append(fixed_rocks)
+    return fixed_rocks_by_orientation
+
+def solve(fixed_rocks, moving_rocks, rows, cols):
+    TARGET_CYCLES = 1000000000
+
+    fixed_rocks_by_orientation = calculate_fixed_rocks_rotations(fixed_rocks, rows, cols)
     cycles = {}
     for i in range(TARGET_CYCLES):
-        tilt(problem)
-        hashed_problem = problem_to_hashable(problem)
-        if hashed_problem in cycles:
+        moving_rocks = tilt_and_rotate_360(fixed_rocks_by_orientation, moving_rocks, rows, cols)
+        hashed_moving_rocks = sparse_to_hashable(moving_rocks)
+        if hashed_moving_rocks in cycles:
             break
-        cycles[hashed_problem] = i
+        cycles[hashed_moving_rocks] = i
 
-    cycle_start = cycles[hashed_problem]
+    cycle_start = cycles[hashed_moving_rocks]
     cycle_length = i - cycle_start
-    remaining_cycles = TARGET_CYCLES - i - 1
+    remaining_cycles = TARGET_CYCLES - i - 1 # -1 because we started counting from 0
     remainder_cycles = cycle_start + (remaining_cycles % cycle_length)
     hashed_in_remainder_cycles = next(k for k, v in cycles.items() if v == remainder_cycles)
-    problem_in_target_cycle = parse_input(hashed_in_remainder_cycles)
-    return count_load(problem_in_target_cycle)
+    moving_rocks_in_target_cycle = hashable_to_sparse(hashed_in_remainder_cycles)
+    return calculate_load(moving_rocks_in_target_cycle, rows)
 
 problem = parse_input(sys.stdin.readlines())
-print(solve(problem))
+print(solve(*problem))
