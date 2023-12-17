0a0691f32e648c5e2b6a6ed3c2b0ab2d3eac4d0e
Alejandro Carrasco <alejandro.carrasco@gmail.com>
2023-12-17 07:38:31 +0100

    Year 2023 - day 17
    It was about time we got an A* search based problem! :D
    
    I just discovered that the implementation of A* for Python that I had
    borrowed long ago and kept reusing was horrible; its performance was
    degrading the bigger the open set got, because it used a simple "min"
    function to get the next candidate to explore. I fixed it using a heap,
    and what a difference!
    
    Part 1 using "min" took 50s, using a heap took 1s.
    Part 2 using "min" took 9m29s, using a heap took 3.6s
    
    Once again, the trickiest part (besides the A* optimization) was an
    off-by-one error with the number of steps that you can take in a
    straight line.
    
          --------Part 1--------   --------Part 2--------
    Day       Time   Rank  Score       Time   Rank  Score
     17   01:15:13   1826      0   01:35:11   1770      0
    
    I need to go back to last year's elephants and valves problem, I think
    it could have been taking forever to execute because of this uO_o

---
+++ 2023/day17/part2.py
@@ -0,0 +1,108 @@
+import sys
+from collections import namedtuple
+from math import inf
+
+State = namedtuple('State', 'i j d s'.split())
+
+def a_star(start, is_goal, neighbors, d, h):
+    from math import inf
+    from collections import defaultdict
+    import heapq
+
+    g_score = defaultdict(lambda: inf)
+    g_score[start] = 0
+
+    f_score = defaultdict(lambda: inf)
+    f_score[start] = h(start)
+
+    open_set = [(f_score[start], start)]
+
+    while open_set:
+        _, current = heapq.heappop(open_set)
+        if is_goal(current):
+            return g_score[current]
+
+        for neighbor in neighbors(current):
+            tentative_g_score = g_score[current] + d(current, neighbor)
+            if tentative_g_score < g_score[neighbor]:
+                g_score[neighbor] = tentative_g_score
+                fs = tentative_g_score + h(neighbor)
+                if fs != inf:
+                    f_score[neighbor] = fs
+                heapq.heappush(open_set, (f_score[neighbor], neighbor))
+    return None
+
+NORTH = (-1, 0)
+SOUTH = (1, 0)
+EAST = (0, 1)
+WEST = (0, -1)
+
+LEFT = {
+    NORTH: WEST,
+    WEST: SOUTH,
+    SOUTH: EAST,
+    EAST: NORTH
+}
+
+RIGHT = {v:k for k, v in LEFT.items()}
+
+MIN_STRAIGHT_STEPS = 4
+MAX_STRAIGHT_STEPS = 10
+
+def simulate(problem):
+    rows, cols = len(problem), len(problem[0])
+
+    def inside(i, j):
+        return 0 <= i < rows and 0 <= j < cols
+
+    def neighbors(state):
+        i, j, d, s = state.i, state.j, state.d, state.s
+
+        if not d:
+            for di, dj in [SOUTH, EAST]:
+                yield State(i=i+di, j=j+dj, d=(di, dj), s=1)
+            return
+
+        # straight
+        if s < MAX_STRAIGHT_STEPS:
+            di, dj = d
+            ni, nj = i + di, j + dj
+            if inside(ni, nj): 
+                yield State(i=ni, j=nj, d=d, s=s+1)
+
+        if s >= MIN_STRAIGHT_STEPS:
+            # left
+            li, lj = LEFT[d]
+            ni, nj = i + li, j + lj
+            if inside(ni, nj):
+                yield State(i=ni, j=nj, d=(li, lj), s=1)
+            # right
+            ri, rj = RIGHT[d]
+            ni, nj = i + ri, j + rj
+            if inside(ni, nj):
+                yield State(i=ni, j=nj, d=(ri, rj), s=1)
+
+    def d(_current, neighbor):
+        return problem[neighbor.i][neighbor.j]
+
+    def h(state):
+        return ((rows - state.i) + (cols - state.j))
+    
+    def is_goal(state):
+        return state.i == rows - 1 and state.j == cols - 1 and state.s >= MIN_STRAIGHT_STEPS
+
+    return a_star(State(i=0, j=0, d=None, s=None), is_goal, neighbors, d, h)
+
+
+def parse_line(line):
+    return [int(x) for x in line.strip()]
+
+def parse(lines):
+    return [parse_line(line) for line in lines]
+
+def solve(problem):
+    solution = simulate(problem)
+    return solution
+
+input = sys.stdin.readlines()
+print(solve(parse(input)))
