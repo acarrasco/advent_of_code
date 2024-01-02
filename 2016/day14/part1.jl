638422e206f7288e0be93523edfbc8f10dd075a3
Alejandro Carrasco <alejandro.carrasco@gmail.com>
2024-01-02 16:27:45 +0100

    Year 2016 - day 14
    I made an "off-by-one" error in the `consecutive` function; very tricky
    to spot because it only failed if the repeated characters were at the
    end of the string.

---
+++ 2016/day14/part1.jl
@@ -0,0 +1,42 @@
+using MD5
+using DataStructures
+
+function consecutive(txt::AbstractString, n::Int)::Union{Char, Missing}
+    for i = 1:length(txt) - n + 1
+        c = txt[i]
+        if all(txt[j] == c for j=i+1:i+n-1)
+            return c
+        end    
+    end
+    return missing
+end
+
+LOOKAHEAD = 1000
+NEEDED = 64
+FIRST_CONSECUTIVE = 3
+NEXT_CONSECUTIVE = 5
+
+function solve(salt::AbstractString)::UInt64
+    buffer = CircularBuffer(LOOKAHEAD)
+    for i = 1:LOOKAHEAD
+        push!(buffer, bytes2hex(md5("$salt$i")))
+    end
+    remaining = NEEDED
+    i = 0
+    while remaining > 0
+        i += 1
+        h = popfirst!(buffer)
+        push!(buffer, bytes2hex(md5("$salt$(i+LOOKAHEAD)")))
+        c = consecutive(h, FIRST_CONSECUTIVE)
+        if c !== missing
+            rc = repeat(c, NEXT_CONSECUTIVE)
+            if any(contains(nh, rc) for nh in buffer)
+                remaining -= 1
+            end
+        end
+    end
+    return i
+end
+
+println(solve("jlmsuwbz"))
+# println(solve("abc"))
