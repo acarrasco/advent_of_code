c38dbadd1ba851bcd62f877c51fe8fcc8b4d19d8
Alejandro Carrasco <alejandro.carrasco@gmail.com>
2024-01-03 00:45:07 +0100

    Year 2016 - day 15
    I must confess that I always struggled with the chinese reminder
    theorem. It was one of those things that wouldn't stick in my head...
    
    But at least I could notice immediately that I needed it here!
    
    I implemented it (cool thing about Julia, it has the `invmod` built in),
    but it wasn't giving the correct answer. I did a second version using a
    sieve, which yielded the same result. I did a brute force version, and
    the solution was different (and correct).
    
    I noticed that this solution was the cycle period minus the other
    analytic solutions... Why was that? Oh, of course, because I was
    calculating the congruence with the offsets *after* the open slot, but I
    should have done it with the offsets *until* getting to the slot again!
    
    For example, if I have a wheel of size 6 and offset (1+1=2), and a wheel
    of size 5 and offset (1+2=3); solving the diophantine equation gives
    offset 8, but I should have solved it for -2%6 (=4) and -3%5 (=2)!

---
+++ 2016/day15/part2.jl
@@ -0,0 +1,29 @@
+
+struct Disc
+    period::Int
+    offset::Int
+end
+
+function parse_line(line::AbstractString)::Disc
+    tokens = split(line)
+    period = parse(Int, tokens[4])
+    offset = parse(Int, chop(tokens[12]))
+    return Disc(period, offset)
+end
+
+function solve(discs::AbstractVector{Disc})
+    period = 1
+    for disc in discs
+        period *= disc.period
+    end
+    offset = 0
+    for (i, disc) in enumerate(discs)
+        m = div(period, disc.period)
+        offset += (i + disc.offset) * m * invmod(m, disc.period)
+    end
+    return period - (offset % period)
+end
+
+discs = map(parse_line, readlines())
+push!(discs, Disc(11, 0))
+println(solve(discs))
