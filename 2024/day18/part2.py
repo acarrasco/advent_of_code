fe66e5b4dd6112778520676648807b33c0342de3
Alejandro Carrasco <alejandro.carrasco@gmail.com>
2024-12-18 06:54:33 +0100

    Year 2024 - Day 18
    Interesting optimization for part 2: we can still use A*, but we
    only have to re-run the path finding if a new wall falls in the
    current best path.
    
    It runs orders of magnitude faster now.

--- 2024/day18/part2.py
+++ 2024/day18/part2.py
@@ -15,35 +15,91 @@ def parse_line(line):
 def parse(lines):
     return [parse_line(line) for line in lines]
 
-def connected(start, end, rows, cols, walls):
-    pending = [start]
-    seen = set()
-    while pending:
-        p = pending.pop()
-        if p == end:
-            return True
+def a_star_gen(start, is_goal, neighbors, d, h):
+    from math import inf
+    from collections import defaultdict
+    import heapq
 
-        i, j = p
-        for di, dj in DIRECTIONS:
-            ni = i + di
-            nj = j + dj
-            np = ni, nj
+    g_score = defaultdict(lambda: inf)
+    g_score[start] = 0
 
-            if 0 <= ni < rows and 0 <= nj < cols and np not in walls and np not in seen:
-                seen.add(np)
-                pending.append(np)
+    f_score = defaultdict(lambda: inf)
+    f_score[start] = h(start)
 
-    return False
+    open_set = [(f_score[start], start)]
+
+    ancestors = defaultdict(set)
+
+    while open_set:
+        _, current = heapq.heappop(open_set)
+        if is_goal(current):
+            yield g_score[current], current, ancestors
+
+        for neighbor in neighbors(current):
+            tentative_g_score = g_score[current] + d(current, neighbor)
+            if tentative_g_score < g_score[neighbor]:
+                ancestors[neighbor] = current
+                g_score[neighbor] = tentative_g_score
+                fs = tentative_g_score + h(neighbor)
+                if fs != inf:
+                    f_score[neighbor] = fs
+                heapq.heappush(open_set, (f_score[neighbor], neighbor))
+    return None
+
+def path(node, ancestors):
+    p = []
+    while node:
+        p.append(node)
+        node = ancestors[node]
+    return p
 
 def solve(problem, fallen_bytes, rows, cols):
+    
     end = rows-1, cols-1
 
     walls = set(b for _, b in zip(range(fallen_bytes), problem))
+
+    def is_goal(state):
+        return end == state
+
+    def h(state):
+        i, j = state
+        end_i, end_j = end
+
+        cost = abs(end_i - i) + abs(end_j - j)
+
+        return cost
+
+    def neighbors(state):
+        i, j = state
+        for di, dj in DIRECTIONS:
+            ni = i + di
+            nj = j + dj
+
+            if 0 <= ni < rows and 0 <= nj < cols and (ni, nj) not in walls:
+                yield (ni, nj)
+
+    def d(current, neighbor):
+        ci, cj = current
+        ni, nj = neighbor
+
+        r = abs(ci - ni) + abs(cj - nj)
+        return r
+
+    _, _, ancestors = next(a_star_gen((0, 0), is_goal, neighbors, d, h))
+    best_path = set(path(end, ancestors))
+
     for i in range(fallen_bytes, len(problem)):
-        walls.add(problem[i])
-        if not connected((0, 0), end, rows, cols, walls):
-            y, x = problem[i]
-            return f'{x},{y}'
+        new_wall = problem[i]
+        walls.add(new_wall)
+        if new_wall in best_path:
+            solution = next(a_star_gen((0, 0), is_goal, neighbors, d, h), None)
+            if not solution:
+                y, x = problem[i]
+                return f'{x},{y}'
+            else:
+                _, _, ancestors = solution
+                best_path = set(path(end, ancestors))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--bytes', type=int, default=1024)
